93d4a23407b9eaa7a3286bfa1d2582f47c53c77c
baptistegoncalves <baptiste.goncalves@epitech.digital>
2025-04-16 07:13:21 +0200

    FIX vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/mode-toggle";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,10 +40,9 @@ export default function RootLayout({
           <div className="fixed top-4 right-4 z-50">
             <ModeToggle />
           </div>
-          <main>
-            {children}
-          </main>
+          <main>{children}</main>
           <Footer />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
