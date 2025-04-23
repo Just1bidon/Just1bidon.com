8ea6f3fdd2fdf85c5d4c2079a8efff935b2e5115
baptistegoncalves <baptiste.goncalves@epitech.digital>
2025-04-23 21:34:31 +0200

    FIX deploy vercel

--- src/app/redirect/page.tsx
+++ src/app/redirect/page.tsx
@@ -1,15 +1,19 @@
 import { redirect } from "next/navigation";
 
-export default function RedirectPage({
+export default async function Page({
   searchParams,
 }: {
-  searchParams: { to?: string };
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
+  // Récupérer le paramètre 'to'
+  const { to } = await searchParams;
+  const toStr = typeof to === "string" ? to : undefined;
+
   // Par défaut, redirige vers localhost
   let target = "http://localhost:3000/music/transfer";
 
   // Redirection vers la prod si précisé
-  if (searchParams.to === "prod") {
+  if (toStr === "prod") {
     target = "https://just1bidon.com/music/transfer";
   }
   // Ajoute d'autres cas si besoin
